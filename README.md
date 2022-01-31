2cadb59bafe3cdf1ec34a1003f1bafcb526c42fe
Arturo Castillo <arturguay@gmail.com>
2022-01-31 14:02:49 -0600

    Added Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+#This is my Booking and listings API
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from matplotlib.pyplot import title
 from fastapi import FastAPI
 from core.config import settings
 
+#comment
+
 # from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI(title=settings.PROJECT_TITLE, description=settings.PROJECT_DESCRIPTION, version=settings.PROJECT_VERSION)
