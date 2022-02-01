5a81c99e10a6a96054623ca016b3648bc43d96f7
Arturo Castillo <arturguay@gmail.com>
2022-01-31 22:50:54 -0600

    Created first schema

--- db/models/users.py
+++ db/models/users.py
@@ -4,15 +4,14 @@ from sqlalchemy.orm import relationship
 from db.base_class import Base
 
 class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(64), nullable=False, index=True)
-    name = Column(String(64), nullable=False, index=True)
-    email = Column(String(64), nullable=False, index=True, unique=True)
-    is_active = Column(Boolean, nullable=False, default=True)
-    is_admin = Column(Boolean, nullable=False, default=False)
-    password = Column(String(128), nullable=False)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
-    rooms = relationship("Room", back_populates="owner")
+    __tablename__   = "users"
+    id              = Column(Integer, primary_key=True, index=True)
+    name            = Column(String(64), nullable=False, index=True)
+    email           = Column(String(64), nullable=False, index=True, unique=True)
+    is_active       = Column(Boolean, nullable=False, default=True)
+    is_admin        = Column(Boolean, nullable=False, default=False)
+    password        = Column(String(128), nullable=False)
+    created_at      = Column(DateTime, nullable=False)
+    updated_at      = Column(DateTime, nullable=False)
+    rooms           = relationship("Room", back_populates="owner")
      
---
+++ schemas/users.py
@@ -0,0 +1,6 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
